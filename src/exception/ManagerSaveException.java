c52b3540f824237b229d3efce25254db1eb68ade
Phantast <serion525@gmail.com>
2024-05-28 19:11:40 +0300

    fix: удалить лишний конструктор

--- src/exception/ManagerSaveException.java
+++ src/exception/ManagerSaveException.java
@@ -2,10 +2,6 @@ package exception;
 
 public class ManagerSaveException extends RuntimeException {
 
-    public ManagerSaveException(String message) {
-        super(message);
-    }
-
     public ManagerSaveException(String message, Throwable e) {
         super(message, e);
     }
