cee876b82ad8e0eaa9889d9a5ad390af2fc74535
Fantasm <serion525@gmail.com>
2024-06-24 23:51:22 +0300

    refactor: Обновить конвертер для задач со временем

--- src/util/TaskToStringConverter.java
+++ src/util/TaskToStringConverter.java
@@ -4,8 +4,20 @@ import model.Task;
 
 public class TaskToStringConverter {
     public static String toString(Task task) {
-        return String.format("%d,%s,%s,%s,%s,%d",
-                task.getId(), task.getType(), task.getTitle(),
-                task.getStatus(), task.getDescription(), task.getEpicTaskId());
+        if (task.getStartTime() != null) {
+            return String.format("%d,%s,%s," +
+                            "%s,%s,%d," +
+                            "%d,%s",
+                    task.getId(), task.getType(), task.getTitle(),
+                    task.getStatus(), task.getDescription(), task.getEpicTaskId(),
+                    task.getDuration().toMinutes(), task.getStartTime());
+        } else {
+            return String.format("%d,%s,%s," +
+                            "%s,%s,%d," +
+                            "%s,%s",
+                    task.getId(), task.getType(), task.getTitle(),
+                    task.getStatus(), task.getDescription(), task.getEpicTaskId(),
+                    null, null);
+        }
     }
 }
