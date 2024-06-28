d0a94a67f6933c107da55ef5ae03a0bb58e9b779
Fantasm <serion525@gmail.com>
2024-06-28 19:16:59 +0300

    feature: Добавить JSON токены типов для списков задач, эпиков и подзадач

---
+++ src/type_token/SubTaskListTypeToken.java
@@ -0,0 +1,9 @@
+package type_token;
+
+import com.google.gson.reflect.TypeToken;
+import model.SubTask;
+
+import java.util.List;
+
+public class SubTaskListTypeToken extends TypeToken<List<SubTask>> {
+}
