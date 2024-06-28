27ee552f10fa221b46def6237cb02c55bdaf31dd
Fantasm <serion525@gmail.com>
2024-06-28 19:18:19 +0300

    feature: Добавить JSON токен типов для списка эпиков

---
+++ src/type_token/EpicTaskTypeToken.java
@@ -0,0 +1,9 @@
+package type_token;
+
+import com.google.gson.reflect.TypeToken;
+import model.EpicTask;
+
+import java.util.List;
+
+public class EpicTaskTypeToken extends TypeToken<List<EpicTask>> {
+}
